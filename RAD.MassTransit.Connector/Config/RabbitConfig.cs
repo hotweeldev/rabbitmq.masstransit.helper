412c99b017978853d04b028346c62ccb93050a28
Nizar Hilmi <nizar.hilmi@bni.co.id>
2025-02-08 13:14:11 +0700

    update queue to handle dlx exchange

--- RAD.MassTransit.Connector/Config/RabbitConfig.cs
+++ RAD.MassTransit.Connector/Config/RabbitConfig.cs
@@ -6,10 +6,21 @@
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public IList<Consumer> Consumers { get; set; } = new List<Consumer>();
+        public IList<DlxConsumer> DlxConsumer { get; set; } = new List<DlxConsumer>();
     }
 
     public class Consumer
     {
+        public string QueueName { get; set; } = string.Empty;
+        public int MaxConcurrent { get; set; } = 1;
+        public string QueueAssembly { get; set; } = string.Empty;
+        public string QueueService {  get; set; } = string.Empty;
+    }
+
+    public class DlxConsumer
+    {
+        public string DlxQueueName { get; set; } = string.Empty;
+        public string DlxName {  get; set; } = string.Empty;
         public string QueueName { get; set; } = string.Empty;
         public int MaxConcurrent { get; set; } = 1;
         public string QueueAssembly { get; set; } = string.Empty;
--- RAD.MassTransit.Connector/Extension/RabbitMQExtension.cs
+++ RAD.MassTransit.Connector/Extension/RabbitMQExtension.cs
@@ -22,12 +22,25 @@ namespace RAD.MassTransit.Connector.Extension
                     x.AddConsumer(consumerType);
                 }
 
+                foreach (var dlxQueue in config.DlxConsumer)
+                {
+                    var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                        .FirstOrDefault(a => a.GetName().Name == dlxQueue.QueueAssembly)
+                        ?? throw new InvalidOperationException($"Assembly '{dlxQueue.QueueAssembly}' could not be loaded.");
+
+                    var consumerType = assembly.GetType(dlxQueue.QueueService)
+                        ?? throw new InvalidOperationException($"Consumer type '{dlxQueue.QueueService}' could not be resolved in assembly '{dlxQueue.QueueAssembly}'.");
+
+                    x.AddConsumer(consumerType);
+                }
+
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(config.Host, h =>
                     {
                         h.Username(config.Username);
                         h.Password(config.Password);
+                        h.Heartbeat(TimeSpan.FromSeconds(30));
                     });
 
                     foreach (var queue in config.Consumers)
@@ -40,9 +53,41 @@ namespace RAD.MassTransit.Connector.Extension
                         {
                             e.Durable = true;
                             e.ConcurrentMessageLimit = queue.MaxConcurrent;
-
                             e.ConfigureConsumer(context, consumerType);
                         });
+
+                        cfg.UseMessageRetry(retryConfig =>
+                        {
+                            retryConfig.Incremental(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5));
+                        });
+                    }
+
+                    foreach (var dlxQueue in config.DlxConsumer)
+                    {
+                        var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                            .FirstOrDefault(a => a.GetName().Name == dlxQueue.QueueAssembly);
+                        var consumerType = assembly?.GetType(dlxQueue.QueueService);
+
+                        cfg.ReceiveEndpoint(dlxQueue.DlxQueueName, e =>
+                        {
+                            e.ConfigureConsumeTopology = false;
+                            e.SetQueueArgument("x-dead-letter-exchange", dlxQueue.DlxName);
+                            e.SetQueueArgument("x-dead-letter-routing-key", dlxQueue.QueueName);
+                            e.SetQueueArgument("x-queue-mode", "lazy");
+                        });
+
+                        cfg.ReceiveEndpoint(dlxQueue.QueueName, e =>
+                        {
+                            e.Durable = true;
+                            e.ConcurrentMessageLimit = dlxQueue.MaxConcurrent;
+                            e.ConfigureConsumer(context, consumerType);
+                        });
+
+                        cfg.UseMessageRetry(retryConfig =>
+                        {
+                            retryConfig.Incremental(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5));
+                        });
+
                     }
                 });
 
